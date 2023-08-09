6f831d3b4ec15c8ceff2bee7c3a6ffec69cc97ec
DynamicCatalyst <manpreet71221@gmail.com>
2023-08-09 19:13:13 +0530

    palindrome method added

--- src/Palindromeint.java
+++ src/Palindromeint.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class Palindromeint {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
         System.out.println("hello world");
+        System.out.println("Is x a Palindrome? = ");
+        boolean a = palindrome(x);
+        System.out.println(a);
+    }
+    public static boolean palindrome(int x){
+
+        boolean f = false;
+        int  temp=x;
+        int r,sum=0;
+        while(x>0){
+            r=x%10;
+            sum=(sum*10)+r;
+            x=x/10;
+        }
+        if(sum==temp)
+            f=true;
+        //System.out.println("f = "+ f);
+        return f;
     }
 }
