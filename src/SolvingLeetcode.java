1e4e58e936ec985a301d1a714fe187047f826d84
DynamicCatalyst <manpreet71221@gmail.com>
2023-08-12 16:13:17 +0530

    LongestCommonPrefix method added

--- src/SolvingLeetcode.java
+++ src/SolvingLeetcode.java
@@ -2,12 +2,34 @@ import java.util.Scanner;
 public class SolvingLeetcode {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number to check if it is a palindrome? = ");
         int x = scanner.nextInt();
         System.out.println("hello world");
         System.out.println("Is x a Palindrome? = ");
         boolean a = palindrome(x);
         System.out.println(a);
+        String word[] = {"abc","abd","ad"};
+        String bro = LongestCommonPrefix(word);
+        System.out.print("longest common prefix is = ");
+        System.out.println(bro);
+
     }
+
+    public static String LongestCommonPrefix(String words[]){
+
+        if(words.length==0) {return  "";}
+        String prefix = words[0];
+        for (int i=1;i<words.length;i++)
+        {
+            while (words[i].indexOf(prefix)!=0)
+            {
+                prefix = prefix.substring(0,prefix.length()-1);
+            }
+        }
+
+        return prefix;
+    }
+
     public static boolean palindrome(int x){
 
         boolean f = false;
